e9d0a3974e22e804a1f8b615b2f8efb7ea393ce6
Mike De Keukeleire <mike.dekeukeleire@student.hogent.be>
2022-07-13 22:06:24 +0200

    services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,29 +1,101 @@
+import {
+  FaRobot,
+  FaBlind,
+  FaCalendarAlt,
+  FaBrain,
+  FaSmile,
+  FaBitcoin,
+  FaRegObjectGroup,
+  FaStreetView,
+  FaInternetExplorer,
+} from "react-icons/fa";
+
 export const Services = (props) => {
   return (
-    <div id='services' className='text-center'>
-      <div className='container'>
-        <div className='section-title'>
-          <h2>Our Services</h2>
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Sessies</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Een humoristische ethische realistische en futuristische kijk op de
+            toekomst
           </p>
         </div>
-        <div className='row'>
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-4'>
-                  {' '}
-                  <i className={d.icon}></i>
-                  <div className='service-desc'>
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
-                </div>
-              ))
-            : 'loading'}
+        <div className="row">
+          <div className="col-md-4">
+            <i className="fa">
+              <FaRobot />
+            </i>
+            <div className="service-desc">
+              <h3>Robots</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaBlind />
+            </i>
+            <div className="service-desc">
+              <h3>Technologie op grootmoeders wijze</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaCalendarAlt />
+            </i>
+            <div className="service-desc">
+              <h3>Het eeuwige leven binnen handbereik</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaBrain />
+            </i>
+            <div className="service-desc">
+              <h3>Kunstmatige intelligentie</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaSmile />
+            </i>
+            <div className="service-desc">
+              <h3>Me, myself and my avatar</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaBitcoin />
+            </i>
+            <div className="service-desc">
+              <h3>Cryptomunten en bitcoins</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaRegObjectGroup />
+            </i>
+            <div className="service-desc">
+              <h3>3D printing</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaStreetView />
+            </i>
+            <div className="service-desc">
+              <h3>Virtual en Augemented reality</h3>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <i className="fa">
+              <FaInternetExplorer />
+            </i>
+            <div className="service-desc">
+              <h3>Web3 en de Metaverse</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
