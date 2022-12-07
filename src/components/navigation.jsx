5bf5526ca103034d2f19a36ba29a0786ec196c35
Mike De Keukeleire <mike.dekeukeleire@student.hogent.be>
2022-12-07 10:14:00 +0100

    last changes

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,18 +5,6 @@ export const Navigation = (props) => {
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
         <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>
           <a className="page-scroll" href="#page-top">
             <img src={logo} style={{ maxWidth: 150 }} />
           </a>{" "}
